c7722b70b6b82ba1f1c13384e4076c9da377db2c
Hunter Leeves <hunterleeves@gmail.com>
2019-01-03 19:57:15 -0600

    minor db syntax

--- database/main.js
+++ database/main.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 var MongoClient = require('mongodb').MongoClient;
 
-var uri = "mongodb+srv://foyy:Fjfjfjfj8!@cluster0.mongodb.net/admin";
+var uri = "mongodb+srv://testing@cluster0.mongodb.net/admin";
 // MongoClient.connect(uri, function (err, client) {
 //   const collection = client.db("test").collection("devices");
 //   // perform actions on the collection object
